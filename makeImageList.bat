53c9550bd53430f012efc7e3a762e493eb0d8ac4
kcross4005 <kcross@san.rr.com>
2023-08-11 15:03:28 -0700

    Refactoring, improve makeImageList.bat

--- makeImageList.bat
+++ makeImageList.bat
@@ -1,3 +1,4 @@
+@echo off
 rem - 20230811 - batch file to list images in the images directory to a file in the data folder
-echo images > data/images.csv
+echo Name> data/images.csv
 dir /b images >> data/images.csv
